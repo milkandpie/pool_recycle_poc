7c279b7f311d819486ea4c6f5e86ddc9c96c7b7c
dungpq <phanquangdng@gmail.com>
2021-05-13 14:39:28 +0700

    feat: add use lifo queue

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
-from create_app import Factory
+from factories import Factory
+from dotenv import load_dotenv
 
+load_dotenv()
 app = Factory().create_app()
+if __name__ == '__main__':
+    app.run()
